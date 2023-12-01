0ddaf4a89e7a9b46e258bae81c938fc49cb38c88
weslemarques <wesleremarques@gmail.com>
2023-11-30 22:34:29 -0300

    refactoring implementation

--- book-service/src/main/java/br/com/wesle/bookservice/model/Book.java
+++ book-service/src/main/java/br/com/wesle/bookservice/model/Book.java
@@ -1,20 +1,24 @@
 package br.com.wesle.bookservice.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
 
 
 @Data
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String author;
     private Date launchDate;
     private Double price;
     private String title;
-    private String currency;
-    private String environment;
 
     public Book() {
     }
