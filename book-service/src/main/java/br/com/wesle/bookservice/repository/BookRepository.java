3cb27fb160e416eeb8f580844c283e44f37e98f0
weslemarques <wesleremarques@gmail.com>
2023-12-04 22:06:17 -0300

    changes layers

--- book-service/src/main/java/br/com/wesle/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/br/com/wesle/bookservice/repository/BookRepository.java
@@ -3,5 +3,5 @@ package br.com.wesle.bookservice.repository;
 import br.com.wesle.bookservice.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book,Long> {
 }
---
+++ book-service/src/main/java/br/com/wesle/bookservice/service/BookService.java
@@ -0,0 +1,41 @@
+package br.com.wesle.bookservice.service;
+
+import br.com.wesle.bookservice.dtos.BookResponseDTO;
+import br.com.wesle.bookservice.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import javax.print.DocFlavor;
+import java.util.Date;
+
+@Service
+public class BookService {
+
+    private final BookRepository repository;
+
+    private final Environment environment;
+    public BookService(BookRepository repository, Environment environment) {
+        this.repository = repository;
+        this.environment = environment;
+    }
+
+    public BookResponseDTO getBookById(Long id){
+        var bookEntity = repository.getReferenceById(id);
+
+
+
+
+        var port = environment.getProperty("local.server.port");
+        return BookResponseDTO.builder()
+                .title(bookEntity.getTitle())
+                .price(bookEntity.getPrice())
+                .id(bookEntity.getId())
+                .author(bookEntity.getAuthor())
+                .launchDate(bookEntity.getLaunchDate())
+                .currency("USD")
+                .environment(port)
+                .build();
+    }
+}
+
