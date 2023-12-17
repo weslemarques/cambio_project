ccd99d3ad7981a8e15006bdf85da0a6782bd687a
weslemarques <wesleremarques@gmail.com>
2023-12-16 22:40:22 -0300

    ajusting configs

--- cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
+++ cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -1,7 +1,6 @@
-﻿CREATE TABLE cambio (
-    id serial PRIMARY KEY ,
-    from_currency CHAR(3) NOT NULL,
-    to_currency CHAR(3) NOT NULL,
-    conversion_factor decimal(10,2) NOT NULL
-);
-
+﻿CREATE TABLE cambio(
+                         id serial PRIMARY KEY ,
+                         from_currency CHAR(3) NOT NULL,
+                         to_currency CHAR(3) NOT NULL,
+                         conversion_factor decimal(10,2) NOT NULL
+ );
